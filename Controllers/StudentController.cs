eadba42c417de1107d862e7341d52c1500eaf23f
ebuaa <axel.aube@viacesi.fr>
2024-08-26 11:21:35 +0200

    First commit

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManager.Models;
+using StudentManager.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace StudentManager.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+    }
+}
---
+++ Models/StudentMdodel.cs
@@ -0,0 +1,13 @@
+﻿using static Microsoft.AspNetCore.Razor.Language.TagHelperMetadata;
+using StudentManager.Models
+
+
+namespace StudentManager.Models
+{
+    public class StudentMdodel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
